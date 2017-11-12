5484ecff9967e4950d30fb9ced3307bc3d3e657d
Matija Puzjak <puzjakmatija@gmail.com>
2017-11-12 17:02:08 +0100

    Added image tag

--- Zpr.Fer.Hr.Lumen/Zpr.Fer.Hr.Lumen/Zpr.Fer.Hr.Lumen/MainPage.xaml.cs
+++ Zpr.Fer.Hr.Lumen/Zpr.Fer.Hr.Lumen/Zpr.Fer.Hr.Lumen/MainPage.xaml.cs
@@ -13,5 +13,10 @@ namespace Zpr.Fer.Hr.Lumen
         {
             InitializeComponent();
         }
+
+        private void PanGestureRecognizer_PanUpdated(object sender, PanUpdatedEventArgs e)
+        {
+
+        }
     }
 }
