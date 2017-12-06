0565033b3490fc78ee34613e799a0dad8b11e4cd
Matija Puzjak <puzjakmatija@gmail.com>
2017-12-06 22:34:08 +0100

    Added Word model, LumenDatabase class and database initialization with test data

---
+++ Zpr.Fer.Hr.Lumen/Zpr.Fer.Hr.Lumen/Zpr.Fer.Hr.Lumen/Models/Word.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zpr.Fer.Hr.Lumen.Models
+{
+    public class Word
+    {
+        [PrimaryKey]
+        public int ID { get; set; }
+        public string ImagePath { get; set; }
+        public int DifficultyID { get; set; }
+        public DateTime LastUsed { get; set; }
+    }
+}
